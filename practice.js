bbdc0094cc82b5e0cb70fac65ee1a0c6360b4384
lilyhuang888 <lilyhuanghaha4ever@gmail.com>
2017-01-20 15:40:56 +0800

    Dependncy Injection

--- practice.js
+++ practice.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+
+(function(){
+	
+	var app = angular.module('myApp', []);
+	
+	app.service('WorldService', function(){
+		this.name = "world";
+	});
+	
+	app.service('JackService', function(){
+		this.name = "Jack";
+	});
+	
+	app.controller('MainController', ['JackService', function(NameService){
+		
+		console.log(NameService.name);
+									  
+	}]);
+	
+})();
